be3b80bfb3f317b73db272a2bbf3d44cc6c75386
Aleks <thirdstrike@yandex.ru>
2020-10-31 20:57:01 +0300

    дЗ-30 - На проверку

--- hw30-executors/src/main/java/ru/otus/threads/Printer.java
+++ hw30-executors/src/main/java/ru/otus/threads/Printer.java
@@ -2,33 +2,37 @@ package ru.otus.threads;
 
 public class Printer implements Runnable {
 
-    private final Integer countLimit;
     private volatile Integer currentCount = 0;
 
-
-    public Printer(Integer countLimit) {
-        this.countLimit = countLimit;
-    }
 
     public synchronized void printNumber(int number) {
         System.out.println(String.format("%s : %d", Thread.currentThread().getName(), number));
         currentCount++;
-        if (currentCount.equals(countLimit)) {
-            currentCount = 0;
-            this.notifyAll();
-        } else {
-            try {
-                this.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        try {
+            this.wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
+
 
     @Override
     public void run() {
         for (int i = 1; i <= 10; i++) {
             printNumber(i);
         }
+    }
+
+    public synchronized Integer getCurrentCount() {
+        return currentCount;
+    }
+
+    public synchronized void setCurrentCount(Integer currentCount) {
+        this.currentCount = currentCount;
+    }
+
+    public synchronized void resetCount() {
+        currentCount = 0;
+        this.notifyAll();
     }
 }
--- hw30-executors/src/main/java/ru/otus/threads/ThreadsApp.java
+++ hw30-executors/src/main/java/ru/otus/threads/ThreadsApp.java
@@ -3,15 +3,29 @@ package ru.otus.threads;
 public class ThreadsApp {
     public static void main(String[] args) throws InterruptedException {
 
-        Printer printer = new Printer(2);
+        int countLimit = 2;
+        Printer printer = new Printer();
         Thread thread1 = new Thread(printer);
         thread1.setName("Thread 1");
 
         Thread thread2 = new Thread(printer);
         thread2.setName("Thread 2");
 
+        Thread thread3 = new Thread(() -> {
+            while (true) {
+                if (printer.getCurrentCount().equals(countLimit)) {
+                    printer.resetCount();
+                }
+                if (!thread1.isAlive() && !thread2.isAlive()) {
+                    return;
+                }
+            }
+        });
+        thread3.setName("Watcher");
+
         thread1.start();
         thread2.start();
+        thread3.start();
 
 
     }
