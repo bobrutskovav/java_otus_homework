0d7b5956d6fbd846720a9f7d46ea0abe23dd4b46
Aleks <thirdstrike@yandex.ru>
2020-11-01 20:46:36 +0300

    дЗ-30 - Работает

--- hw30-executors/src/main/java/ru/otus/threads/Printer.java
+++ hw30-executors/src/main/java/ru/otus/threads/Printer.java
@@ -2,34 +2,30 @@ package ru.otus.threads;
 
 public class Printer implements Runnable {
 
-    private volatile boolean firstThreadWasHere = false;
-    private volatile boolean secondThreadWasHere = false;
+    private volatile boolean isFirstThreadRun = false;
 
 
     public synchronized void performPrint(int number) {
-        if (Thread.currentThread().getName().endsWith(" 2")) {
-            while (!firstThreadWasHere) {
-                waitThread();
-            }
-            printNumber(number);
-            secondThreadWasHere = true;
-            this.notifyAll();
-        }
-        if (Thread.currentThread().getName().endsWith(" 1")) {
-            printNumber(number);
-            firstThreadWasHere = true;
-            this.notifyAll();
-            while (!secondThreadWasHere) {
+        if (!Thread.currentThread().getName().endsWith(" 2")) {
+            while (isFirstThreadRun) {
                 waitThread();
             }
         }
+        if (!Thread.currentThread().getName().endsWith(" 1")) {
+            while (!isFirstThreadRun) {
+                waitThread();
+            }
+        }
+        printNumber(number);
+        isFirstThreadRun = !isFirstThreadRun;
+        this.notifyAll();
 
     }
 
 
     private void waitThread() {
         try {
-            System.out.println(Thread.currentThread().getName() + " wait");
+            // System.out.println(Thread.currentThread().getName() + " wait"); debug sout)
             this.wait();
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -42,18 +38,13 @@ public class Printer implements Runnable {
     }
 
 
-    public synchronized void notifyThreads() {
-        firstThreadWasHere = false;
-        secondThreadWasHere = false;
-        this.notifyAll();
-    }
-
     @Override
     public void run() {
         for (int i = 1; i <= 10; i++) {
             performPrint(i);
-            firstThreadWasHere = false;
-            secondThreadWasHere = false;
+        }
+        for (int i = 10; i >= 0; i--) {
+            performPrint(i);
         }
     }
 }
