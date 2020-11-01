f85b609ba91a9fbd3143285729490f385a2cc0cf
Aleks <thirdstrike@yandex.ru>
2020-11-01 19:12:34 +0300

    дЗ-30 - Не работает2

--- hw30-executors/src/main/java/ru/otus/threads/Printer.java
+++ hw30-executors/src/main/java/ru/otus/threads/Printer.java
@@ -2,13 +2,34 @@ package ru.otus.threads;
 
 public class Printer implements Runnable {
 
-    private volatile Integer currentCount = 0;
+    private volatile boolean firstThreadWasHere = false;
+    private volatile boolean secondThreadWasHere = false;
 
 
-    public synchronized void printNumber(int number) {
-        System.out.println(String.format("%s : %d", Thread.currentThread().getName(), number));
-        currentCount++;
+    public synchronized void performPrint(int number) {
+        if (Thread.currentThread().getName().endsWith(" 2")) {
+            while (!firstThreadWasHere) {
+                waitThread();
+            }
+            printNumber(number);
+            secondThreadWasHere = true;
+            this.notifyAll();
+        }
+        if (Thread.currentThread().getName().endsWith(" 1")) {
+            printNumber(number);
+            firstThreadWasHere = true;
+            this.notifyAll();
+            while (!secondThreadWasHere) {
+                waitThread();
+            }
+        }
+
+    }
+
+
+    private void waitThread() {
         try {
+            System.out.println(Thread.currentThread().getName() + " wait");
             this.wait();
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -16,23 +37,23 @@ public class Printer implements Runnable {
     }
 
 
+    private void printNumber(int number) {
+        System.out.println(String.format("%s : %d", Thread.currentThread().getName(), number));
+    }
+
+
+    public synchronized void notifyThreads() {
+        firstThreadWasHere = false;
+        secondThreadWasHere = false;
+        this.notifyAll();
+    }
+
     @Override
     public void run() {
         for (int i = 1; i <= 10; i++) {
-            printNumber(i);
+            performPrint(i);
+            firstThreadWasHere = false;
+            secondThreadWasHere = false;
         }
-    }
-
-    public synchronized Integer getCurrentCount() {
-        return currentCount;
-    }
-
-    public synchronized void setCurrentCount(Integer currentCount) {
-        this.currentCount = currentCount;
-    }
-
-    public synchronized void resetCount() {
-        currentCount = 0;
-        this.notifyAll();
     }
 }
--- hw30-executors/src/main/java/ru/otus/threads/ThreadsApp.java
+++ hw30-executors/src/main/java/ru/otus/threads/ThreadsApp.java
@@ -3,29 +3,16 @@ package ru.otus.threads;
 public class ThreadsApp {
     public static void main(String[] args) throws InterruptedException {
 
-        int countLimit = 2;
+
         Printer printer = new Printer();
         Thread thread1 = new Thread(printer);
         thread1.setName("Thread 1");
-
         Thread thread2 = new Thread(printer);
         thread2.setName("Thread 2");
 
-        Thread thread3 = new Thread(() -> {
-            while (true) {
-                if (printer.getCurrentCount().equals(countLimit)) {
-                    printer.resetCount();
-                }
-                if (!thread1.isAlive() && !thread2.isAlive()) {
-                    return;
-                }
-            }
-        });
-        thread3.setName("Watcher");
 
         thread1.start();
         thread2.start();
-        thread3.start();
 
 
     }
